0fbac3b11b5e937b9017bd34708ee4e8d6ee1888
Isaac I. <s2500147@edu.bc.fi>
2025-03-03 12:52:52 +0200

    Delete 03_week directory

--- 03_week/03_exercise/loops/loops_04.js
+++
@@ -1,6 +0,0 @@
-/* 
-Write a function named countEvenNumbers:
-- Prompt the user to input 20 numbers one by one.
-- After all numbers are entered, log how many of them were even.
-Display the result in the console. 
-*/
--- 03_week/03_exercise/loops/loops_12.js
+++
@@ -1,6 +0,0 @@
-/* 
-Write a function named buildGroceryList:
-- Prompt the user to input grocery items one by one.
-- Stop when the user types "done".
-Log the complete grocery list in the console.
- */
