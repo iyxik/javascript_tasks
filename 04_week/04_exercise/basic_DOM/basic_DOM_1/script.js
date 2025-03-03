aa6e1cc889b649143e6a9676bc67af5533554416
Isaac I. <s2500147@edu.bc.fi>
2025-03-03 12:53:04 +0200

    Delete 04_week directory

--- 04_week/04_exercise/basic_DOM/basic_DOM_1/script.js
+++
@@ -1,5 +0,0 @@
-/* 
-Use an HTML file with a <p> element that has the ID content.
-Write JavaScript to select this element using getElementById.
-Create a function that changes its text to "Hello, World!" when a button is clicked.
-*/
--- 04_week/04_exercise/basic_DOM/basic_DOM_7/script.js
+++
@@ -1,4 +0,0 @@
-/* 
-Create an HTML file with a button and a counter display.
-Write JavaScript to count and display the number of times the button is clicked. 
-*/
