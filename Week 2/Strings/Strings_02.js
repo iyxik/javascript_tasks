3f9e7e6f63c640e3f51c6824998d9064b09f7def
iyxik <s2500147@edu.bc.fi>
2026-02-16 22:38:58 +0200

    Remove old folders/files

--- Week 2/Strings/Strings_02.js
+++
@@ -1,9 +0,0 @@
-/*
-Declare a variable language and assign it the value "JavaScript".
-Create a string using backticks to say: "I am learning JavaScript." by embedding the language variable into the string.
-Print the resulting string using console.log(). 
-*/
-
-let language = "JavaScript";
-let learn = `I am learning ${language}`;
-console.log(learn);
